bbe59af257d31757c496e6cbd79a5dd8a23edfd7
Norman Hu <normanhu@Normans-MacBook-Pro.local>
2022-03-29 18:06:44 +1100

    Changed weather service to OpenWeather

--- src/utils/forecast.mjs
+++ src/utils/forecast.mjs
@@ -1,10 +1,10 @@
 import request from 'postman-request'
 
-const forecast = (latitude, longitude, callback) => {
+const weatherstackForecast = (latitude, longitude, callback) => {
     const weatherstackURL = 'http://api.weatherstack.com/current?access_key=6ae1e1f3dc367db7cc7dcd0a619c9927&query=' + latitude + ',' + longitude
 
     request({ url: weatherstackURL, json: true }, (error, {
-        body: { error: serviceError },
+        body: { error: { info: serviceError } = {} },
         body: { location },
         body: { current: { weather_icons, weather_descriptions, temperature, feelslike } = {} }
     }) => {
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Could not connect to weather service', undefined)
         }
         else if (serviceError) {
-            callback('Invalid request to weather service', undefined)
+            callback(serviceError, undefined)
         }
         else {
             callback(undefined, {
@@ -26,4 +26,31 @@ const forecast = (latitude, longitude, callback) => {
     })
 }
 
-export default forecast
+const openweatherForecast = (latitude, longitude, callback) => {
+    const openweatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=6b2907edd06afa3b8bad8432fd08391e'
+
+    request({ url: openweatherURL, json: true }, (error, {
+        body: { message: serviceError },
+        body: { name: location },
+        body: { weather: [{ description, icon }] },
+        body: { main: { temp, feels_like } }
+    }) => {
+        if (error) {
+            callback('Could not connect to weather service', undefined)
+        }
+        else if (serviceError) {
+            callback(serviceError, undefined)
+        }
+        else {
+            callback(undefined, {
+                location,
+                weather_icons: "https://openweathermap.org/img/w/" + icon + ".png",
+                weather_descriptions: [description],
+                temperature: temp,
+                feelslike: feels_like
+            })
+        }
+    })
+}
+
+export default openweatherForecast
